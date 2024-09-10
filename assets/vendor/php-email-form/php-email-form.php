307b927bce31cb2136b5209310fd40f9f72b1e13
Andressa172 <andressa.dinizff@gmail.com>
2024-09-09 23:35:31 -0300

    adicionando alguns recursos de acessibilidade

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,21 @@
+<?php
+  $receiving_email_address = 'contact@example.com';
+
+  if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
+    $to = $receiving_email_address;
+    $subject = "Form Submission from " . $_POST['name'];
+    $message = "From: " . $_POST['name'] . "\n";
+    $message .= "Email: " . $_POST['email'] . "\n";
+    $message .= "Message: " . $_POST['message'];
+    $headers = "From: " . $_POST['email'];
+    $subject = "CPF: " . $_POST['cpf'];
+
+    if (mail($to, $subject, $message, $headers)) {
+      echo "Message sent successfully!";
+    } else {
+      echo "Failed to send message.";
+    }
+  } else {
+    echo "Form data missing.";
+  }
+?>
